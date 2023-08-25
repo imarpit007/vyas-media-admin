26009b76bf0b70585a8ee69f79f237a57ed32649
Arpit Gupta <arpit.gupta@nisostech.com>
2023-08-25 17:28:10 +0530

    Added Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'admin/forget-password', component: ForgetPasswordComponent },
   { path: 'admin/login', component: LoginComponent },
   { path: '**', redirectTo: '',},
@@ -14,4 +16,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [ForgetPasswordComponent, LoginComponent]
+export const routingComponents = [ForgetPasswordComponent, LoginComponent, HomeComponent]
