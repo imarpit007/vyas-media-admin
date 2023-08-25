851062997408de0d43b260ab936f6991508cb196
Arpit Gupta <arpit.gupta@nisostech.com>
2023-08-25 20:38:32 +0530

    Added API for reset password

--- src/app/utils/services/api.service.ts
+++ src/app/utils/services/api.service.ts
@@ -11,12 +11,14 @@ export class ApiService {
   login: string;
   checkEmail: string;
   checkOTP: string;
+  resetPsw: string;
 
   constructor(private http: HttpClient) {
     this.apiHost = environment.API_HOST;
     this.login = this.apiHost + `auth/login`;
     this.checkEmail = this.apiHost + `auth/forgetUserPassword`;
     this.checkOTP = this.apiHost + `auth/verifyOTPForgetUserPassword`;
+    this.resetPsw = this.apiHost + `auth/forgetUserPasswordUpdate`;
   }
 
   onLogin(requestParameters : any) {
@@ -29,6 +31,10 @@ export class ApiService {
 
   checkOTPForgotPsw(requestParameters : any) {
     return this.http.post(`${this.checkOTP}`, JSON.parse(requestParameters), {});
+  }
+
+  resetForgotPsw(requestParameters: any) {
+    return this.http.post(`${this.resetPsw}`, JSON.parse(requestParameters), {});
   }
 
 }
