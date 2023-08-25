0f71c34d465d7f0d5a89e6c57ddca4f4714e9f72
Arpit Gupta <arpit.gupta@nisostech.com>
2023-08-25 20:21:20 +0530

    Added reset password page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import { ForgetPasswordComponent } from './pages/forget-password/forget-password
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './views/home/home.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'admin/login', component: LoginComponent },
   { path: 'admin/forget-password', component: ForgetPasswordComponent },
+  // { path: 'admin/reset-password/:emailAdd', component: ResetPasswordComponent },
+  { path: 'admin/reset-password', component: ResetPasswordComponent },
   { path: 'admin/dashboard', component: DashboardComponent },
   { path: '**', redirectTo: '',},
 ];
@@ -19,4 +22,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [ForgetPasswordComponent, LoginComponent, HomeComponent, DashboardComponent]
+export const routingComponents = [HomeComponent, LoginComponent, ForgetPasswordComponent, ResetPasswordComponent, DashboardComponent,]
