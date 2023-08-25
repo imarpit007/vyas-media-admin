44cc49f48bde868a1e62875d578cea53b18599a5
Arpit Gupta <arpit.gupta@nisostech.com>
2023-08-25 14:17:24 +0530

    Added logic for login in login.ts component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, Renderer2 } from '@angular/core';
+import { ApiService } from '../utils/services/api.service';
+import { AppService } from '../utils/services/app.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  // public isAuthLoading = false;
+  constructor(
+    private appService: AppService,
+    private apiService: ApiService,
+  ){}
+
+  ngOnInit() {
+  }
+
+  login(){
+    let loginData = {
+      email: "a@gmail.com",
+      password: "123456"
+    }
+    this.apiService.onLogin(JSON.stringify(loginData)).subscribe((result: any) => {
+      console.log(result);
+        if (result.success) {
+          // this.isAuthLoading = false;
+          this.appService.login(result);
+        } else {
+          // this.isAuthLoading = false;
+        }
+    })
+  }
 
 }
