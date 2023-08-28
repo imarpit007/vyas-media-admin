deb14616e6d4ec3aea86d5b93f836a5cf5d7ee9b
Arpit Gupta <arpit.gupta@nisostech.com>
2023-08-28 14:31:31 +0530

    Added footer in the admin-panel

--- src/app/views/admin/footer/footer.component.ts
+++ src/app/views/admin/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import  packageInfo  from '../../../../../package.json';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  public appVersion = packageInfo.version;
 
 }
